a966fcef032e0663fcac6ebfec1721f08d54027c
IndieForger <indieforger@gmail.com>
2015-12-30 12:24:22 +0000

    path verification has been moved to external module

--- service.js
+++ service.js
@@ -5,50 +5,14 @@ var
 	async = require('async'),
 	fs = require('fs'),
 	path = require('path'),
-	simpleGit = require('simple-git')
-
+	simpleGit = require('simple-git'),
+	verifyPath = require('verify-path')
 
 //setInterval(initCheck, 1000);
 
-verifyPath(config)
+verifyPath(config, parseInt('0755', 8))
 	.then(updateRepositories)
 	.catch((err) => { console.log(err.stack) })
-
-
-/**
- * Verifies the path to directory storing repositories.
- * If directory doesn't exist it will create one.
- * @param settings
- * @returns {Promise}
- */
-function verifyPath(settings) {
-	let dirPath = path.resolve(__dirname, settings.path)
-	return new Promise((resolve, reject) => {
-
-		// check if file exist
-		fs.stat(dirPath, (error, stats) => {
-
-			// create directory if doesn't
-			if (error) {
-				return fs.mkdir(dirPath, parseInt('0755',8), (error) => {
-					if (error) return reject (error)
-					resolve(settings)
-				})
-			}
-
-			// check if is a directory
-			if (!stats.isDirectory()) {
-				return reject(new Error('File: ' + dirPath + ' is not a directory'))
-			}
-
-			// check if is writable
-			return fs.access(dirPath, fs.F_OK | fs.R_OK | fs.W_OK, (error) => {
-				if (error) return reject(error)
-				resolve(settings)
-			})
-		});
-	})
-}
 
 /**
  * Method clones or fast forwards repositories.
