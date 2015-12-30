3f62228eb02c99c98a451f7cd2d84eb9dd9be14b
IndieForger <indieforger@gmail.com>
2015-12-30 15:32:16 +0000

    repomine updated to 1.0.1

--- service.js
+++ service.js
@@ -2,10 +2,10 @@
 var
 	//helpers = require('./lib/helpers.js'),
 	config = require('./config.json'),
-	repomine = require('repomine');
+	repomine = require('repomine'),
+	seneca = require('seneca')();
 
 //setInterval(initCheck, 1000);
-
 
 repomine
 	.update(config)
@@ -13,3 +13,11 @@ repomine
 		console.log(output)
 	})
 	.catch((err) => { console.log(err.stack) })
+
+
+seneca.add({act: 'content', cmd: 'get'}, function (msg, next) {
+	// /games/jewelines/index.html
+
+	msg.uri = '';
+	next();
+})
